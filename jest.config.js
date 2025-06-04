1a6ea2da97a1da8b262fe302b584d9d6ede47abf
anna <125645248+annadoannn@users.noreply.github.com>
2025-06-04 03:29:22 -0700

    Jest configuration file to handle ES modules and run tests

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+export default {
+  transform: {},
+  testEnvironment: 'node'
+//   extensionsToTreatAsEsm: ['.js'],
+};
+// This Jest configuration file is set up to handle ES modules and run tests in a Node environment.
