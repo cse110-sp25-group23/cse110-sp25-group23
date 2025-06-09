7b0dba3c2c5a62d89a53f637f8f3fd285d856f24
Ahmad-180 <114322095+Ahmad-180@users.noreply.github.com>
2025-06-08 22:13:23 -0700

    css and config changes

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl:    "http://localhost:8082",            // your dev server
+    baseUrl:    "http://localhost:8082",            //  dev server
     specPattern: "__tests__/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: false,
   },
